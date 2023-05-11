2b04ab02f8febba39be9af106403f92794c71ed4
Isaac Diaz <isaac.diaz.lopez@hotmail.com>
2023-05-11 21:41:44 +0200

    filtros, Reiniciar app, presupuesto y gastos en localStorage

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,17 +1,36 @@
 import Gasto from "./Gasto"
 
-const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto }) => {
+const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto, filtro, gastosFiltrados }) => {
     return (
         <div className='listado-gastos contenedor'>
-            <h2>{gastos.length ? 'Gastos' : 'No hay gastos que mostrar'}</h2>
-            {gastos.map(gasto => (
-                <Gasto
-                    key={gasto.id}
-                    gasto={gasto}
-                    setGastoEditar={setGastoEditar}
-                    eliminarGasto={eliminarGasto}
-                />
-            ))}
+
+
+            {filtro ? (
+                <>
+                    <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay gastos que mostrar en esta categoria'}</h2>
+                    {gastosFiltrados.map(gasto => (
+                        <Gasto
+                            key={gasto.id}
+                            gasto={gasto}
+                            setGastoEditar={setGastoEditar}
+                            eliminarGasto={eliminarGasto}
+                        />
+                    ))}
+                </>
+            ) : (
+                <>
+                <h2>{gastos.length ? 'Gastos' : 'No hay gastos que mostrar'}</h2>
+                    {gastos.map(gasto => (
+                        <Gasto
+                            key={gasto.id}
+                            gasto={gasto}
+                            setGastoEditar={setGastoEditar}
+                            eliminarGasto={eliminarGasto}
+                        />
+                    ))}
+                </>
+            )
+            }
         </div>
     )
 }
